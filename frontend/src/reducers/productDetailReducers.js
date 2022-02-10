01ce5052af414e79f8dcb7081ae86cb01980b0f3
aniket <aniket shirke>
2022-02-10 15:45:19 +0530

    cors issue for api/products/3

---
+++ frontend/src/reducers/productDetailReducers.js
@@ -0,0 +1,21 @@
+import {
+  PRODUCT_DETAIL_REQUEST,
+  PRODUCT_DETAIL_SUCCESS,
+  PRODUCT_DETAIL_FAIL,
+} from "../constants/productDetailConstants";
+
+export const productDetailReducer = (
+  state = { product: { reviews: [] } },
+  action
+) => {
+  switch (action.type) {
+    case PRODUCT_DETAIL_REQUEST:
+      return { loading: true, ...state };
+    case PRODUCT_DETAIL_SUCCESS:
+      return { loading: false, product: action.payload };
+    case PRODUCT_DETAIL_FAIL:
+      return { loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
